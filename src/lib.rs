dd24d1cd68a8e4eae76dd3c91ee8d45093b2c36d
QuietMisdreavus <grey@quietmisdreavus.net>
2017-03-15 17:25:20 -0500

    add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //! A limited XML representation to aid in serializing/deserializing SOAP messages.
 
 #![warn(missing_docs)]
